0ff433efe8cdeb9d1bc59cb58237a9967ace75b3
khosta77 <puwerfulpants@gmail.com>
2022-05-08 22:21:15 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-//#include "./project/include/Table.h"
-//#include "./project/include/auxiliary_func.h"
-//#include "./project/src/auxiliary_func.cpp"
+#include "./project/include/Table.h"
 #include "./project/include/Tcmd.h"
 
-#include <iostream>
-#include <string>
-
-using namespace std;
 
 int main() {
     Tcmd cmd;
