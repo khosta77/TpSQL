bb7563dad8f2d3e56a0861e6c258c35f466730ed
khosta77 <puwerfulpants@gmail.com>
2022-05-15 17:22:46 +0300

    Слита ветка Данилы

--- project/include/Table.h
+++ project/include/Table.h
@@ -10,7 +10,7 @@ using namespace std;
 
 class Table {
 private:
-    vector<string> table;
+    vector<string> tab; // table
     size_t rows = 0;
     size_t cols = 0;
     //// Test
@@ -19,6 +19,60 @@ public:
 //    Table(string path_file);
 //    ~Table();
 
+
+    //// Конструкторы
+    // Создаем пустую таблицу заданного размера
+    Table(size_t rows, size_t cols);
+
+    // Создаем переменную класса Table
+    Table() = default;
+
+    //// Перегруженные операторы
+    Table& operator=(const Table& rhs);
+
+    //// Сеттеры
+    // Задать количество строк
+    void set_rows_size(size_t rows);
+
+    // Задать количество столбцов
+    void set_cols_size(size_t cols);
+
+    // Заполнение элементов таблицы пользователем
+    void set_table();
+
+    // Заполнение элементов добавленной строки пустыми строками
+    void set_empty_row(size_t row);
+
+    // Заполнение элементов добавленного столбца пустыми строками
+    void set_empty_col(size_t col, size_t current_col);
+
+    // Редактирование содержимого ячейки
+    void set_elem(size_t row, size_t col, string value);
+
+    //// Геттеры
+    // Узнать количество строк
+    size_t get_rows() const;
+
+    // Узнать количство столбцов
+    size_t get_cols() const;
+
+    // Вывод содержимого ячейки
+    string get_elem(size_t row, size_t col);
+
+    //// Остальное
+    // Вывод таблицы в консоль
+    void out_str();
+
+    // Узнаем, пуста ли таблица
+    bool table_empty();
+
+    // Запись таблицы в файл
+    void write_file(string filename);
+
+    // Записи элементов таблицы из файла в вектор
+    void read_file(string path_file);
+
+/*  // Методы реализованы на ветке stepan-2
     // read/write table
     void read_file(string path_file);
     void write_file(string filename);
@@ -39,7 +93,7 @@ public:
     void set_elem(size_t row, size_t col, string value);  // Редактирование содержимого ячейки
 
 
-
+*/
     // !!! dop func
     void push_col(string col_name);
     void set_rows_size();  // Изменение количества строк
--- project/src/Table.cpp
+++ project/src/Table.cpp
@@ -22,6 +22,165 @@ static void split_str_2(string str, vector<string> &array) {
 //Table::Table(string path_file);
 //Table::~Table();
 
+// Создаем пустую таблицу заданного размера
+Table::Table(size_t row, size_t col) {
+    rows = row;
+    cols = col;
+    for (size_t i = 0; i < rows * cols; i++) {
+        tab.push_back("");
+    }
+}
+
+//// Перегруженные операторы
+
+// Опреатор присваивания
+Table& Table::operator=(const Table& rhs) {
+    rows = rhs.get_rows();
+    cols = rhs.get_cols();
+    tab = rhs.tab;
+    return *this;
+}
+
+// Функция записи элементов таблицы из файла в вектор
+void Table::read_file(string path_file) {
+    ifstream fin;
+    fin.open(path_file);
+    string buf;
+    while (!fin.eof()) {
+        getline(fin, buf);
+        if (buf != "") {
+            split_str_2(buf, tab);  // функция разбиения считанной строки на элементы
+            rows++;
+        }
+    }
+    cols = tab.size() / rows;
+    fin.close();
+}
+
+// Вывод таблицы в консоль
+void Table::out_str() {
+    int max_elem_size;
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            // Ищем максимальный элемент в столбце и выводим в консоль ячейки с шириной,
+            // равной ширине максимального жлемента в столце
+            max_elem_size = max_elem_size_in_col(tab, rows, cols, j);
+            cout << setw(max_elem_size)<< left << tab.at(j + i * cols) << "   ";
+        }
+        cout << endl;
+    }
+}
+
+// Вывод содержимого ячейки
+string Table::get_elem(size_t row, size_t col) {
+    if ((row >= rows) || (col >= cols)) {
+        return "Ошибка. Неверно введен номер ячейки";
+    }
+
+    if (tab.at(col + row * cols) == "") {
+        return "";
+    } else {
+        return tab.at(col + row * cols);
+    }
+}
+
+// Редактирование содержимого ячейки
+void Table::set_elem(size_t row, size_t col, string value) {
+    tab.at(col + row * cols) = value;
+}
+
+// Задание содержимого ячеек
+void Table::set_table() {
+    string buf;
+    for (size_t i = 0; i < rows; i++) {
+        cout << "Введите " << i  << " строку. Значение ячеек разделяйте ;" << endl;
+        fill_row(this, i);
+    }
+}
+
+// Задать количество строк
+void Table::set_rows_size(size_t rows) {
+    this->rows = rows;
+}
+
+// Задать количество столбцов
+void Table::set_cols_size(size_t cols) {
+    this->cols = cols;
+}
+
+// Ввести элементы строки
+void Table::set_empty_row(size_t row) {
+    for (size_t i = this->rows; i < (this->rows + row); i++) {
+        for (size_t j = 0; j < cols; j++) {
+            tab.push_back("");
+        }
+    }
+    this->rows = this->rows + row;
+}
+
+// Заполнение элементов добавленного столбца пустыми строками
+void Table::set_empty_col(size_t col, size_t new_col) {
+    vector<string> new_table;
+    // Заполняем построчно
+    for (size_t i = 0; i < rows; i++) {
+        // Добавляем уже существующие элементы
+        for (size_t j = 0; j < col; j++) {
+            new_table.push_back(tab.at(j + i * (col)));
+        }
+
+        if (i == 0) {
+            // Добавляем заголовок
+            for (size_t i = 0; i < new_col; i++) {
+                string heading_num;
+                cout << "Введите название столбца" << endl;
+                cin >> heading_num;
+                new_table.push_back(heading_num);
+            }
+        } else {
+            // Добавляем пустые элементы
+            for (size_t j = 0; j < new_col; j++) {
+                new_table.push_back("");
+            }
+        }
+    }
+
+    tab = new_table;
+}
+
+// Узнать количество строк
+size_t Table::get_rows() const {
+    return rows;
+}
+
+// Узнать количество столбцов
+size_t Table::get_cols() const {
+    return cols;
+}
+
+// Запись таблицы в файл
+void Table::write_file(string filename) {
+    ofstream fout;
+    fout.open(filename);
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            fout << tab.at(j + i * cols);
+
+            if (j + 1 != cols) {
+                fout << ";";
+            }
+        }
+        fout << endl;
+    }
+    fout.close();
+}
+
+// Узнаем, пуста ли таблица
+bool Table::table_empty() {
+    return tab.empty();
+}
+
+
+/*
 // read/write table
 void Table::read_file(string path_file) {
     ifstream fin;
@@ -146,18 +305,18 @@ void Table::set_col_elems(size_t col, size_t current_col) {
 
     table = new_table;
 }
-
+*/ // Методы в stepan-2
 //dop func
 void Table::push_col(string col_name) { // Вынужденный метод на данном этапе.
-    table.push_back(col_name);
-    this->cols = table.size();
+    tab.push_back(col_name);
+    this->cols = tab.size();
 }
 
 void Table::set_rows_size() {
-    if ((this->rows * this->cols) >= table.size()) {
-        table.resize((this->rows + 1) * this->cols);
+    if ((this->rows * this->cols) >= tab.size()) {
+        tab.resize((this->rows + 1) * this->cols);
     }
-    table.at(this->rows * this->cols).push_back('\0');
+    tab.at(this->rows * this->cols).push_back('\0');
     this->rows++;
 //    for (size_t i = this->rows * this->cols - 1; i < rows * this->cols; i++) {
 //        table.push_back(" ");
