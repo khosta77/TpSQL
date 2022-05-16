460678d0f2101fdd1eb2792ec06d225e2bc24d84
khosta77 <puwerfulpants@gmail.com>
2022-05-16 18:57:54 +0300

    ФАЙЛ АААААААА

---
+++ CMakeFiles/TpSQL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TpSQL.dir/main.cpp.o"
+  "CMakeFiles/TpSQL.dir/project/src/Table.cpp.o"
+  "CMakeFiles/TpSQL.dir/project/src/auxiliary_func.cpp.o"
+  "TpSQL"
+  "TpSQL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TpSQL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
